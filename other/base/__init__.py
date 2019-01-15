3b056003fafba653b37a1420fff7baf80a3f9c19
lijiakui <lijk@smartbow.net>
2019-01-15 11:02:21 +0800

    ..

--- other/base/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding:utf-8 -*-
-from rpc_context import RPCContextHandler
--- other/settings.py
+++
@@ -1,7 +0,0 @@
-# -*- coding:utf-8 -*-
-DB_NAME='sensorcmd'
-DB_HOST='192.168.111.149:27019'
-CACHE_HOST='192.168.111.5:6371'
-
-
-
